97468ba7f87cb6d4bf09ac24b68bf8b7dce9612a
dewey-typical <touil.anas.lpcb@gmail.com>
2023-02-15 14:02:43 +0100

    Multi module maven

--- server/src/main/java/lernejo/chat/ChatMessageController.java
+++ server/src/main/java/lernejo/chat/ChatMessageController.java
@@ -1,7 +1,5 @@
-package fr.lernejo.chat;
+package lernejo.chat;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;

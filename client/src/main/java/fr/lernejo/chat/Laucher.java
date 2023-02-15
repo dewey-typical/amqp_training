a7cdf858b48f645225c671bc2207199b83d8782c
dewey-typical <touil.anas.lpcb@gmail.com>
2023-02-15 15:34:01 +0100

    create Client

---
+++ client/src/main/java/fr/lernejo/chat/Laucher.java
@@ -0,0 +1,27 @@
+package fr.lernejo.chat;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class Laucher {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Laucher.class);
+        RabbitTemplate rabbitTemplate = context.getBean(RabbitTemplate.class);
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("Input a message, we will sent it for you (q for quit):\n");
+            String message = sc.nextLine();
+            if (message.equals("q")) {
+                System.out.println("Salam");
+                return;
+            }
+
+            rabbitTemplate.convertAndSend("", "chat_messages", message);
+            System.out.println("message sent ! ");
+        }
+    }
+}
